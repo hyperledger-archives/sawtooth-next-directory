e08b08d7c7ab88d7822f9066d2361196a38bde16
Darian Plumb <dplumb@bitwise.io>
2017-11-20 14:44:09 -0600

    Refactor existing block queries
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- server/db/blocks_query.py
+++ server/db/blocks_query.py
@@ -16,7 +16,7 @@
 import logging
 
 import rethinkdb as r
-from rethinkdb import ReqlNonExistenceError, ReqlCursorEmpty
+from rethinkdb import ReqlNonExistenceError, ReqlRuntimeError
 
 from api.errors import ApiNotFound, ApiInternalError
 
@@ -26,30 +26,45 @@ LOGGER = logging.getLogger(__name__)
 
 async def fetch_latest_block(conn):
     try:
-        result = await r.table('blocks').max('block_num').run(conn)
+        return await r.table('blocks')\
+            .max(index='block_num')\
+            .merge({
+                'id': r.row['block_id'],
+                'num': r.row['block_num']
+            })\
+            .without('block_id', 'block_num').run(conn)
     except ReqlNonExistenceError:
         raise ApiInternalError('Internal Error: No block data found in state')
-    return result
 
 
 async def fetch_block_by_id(conn, block_id):
-    cursor = await r.table('blocks').get_all(
-        block_id, index='block_id'
-    ).run(conn)
+    resource = await r.table('blocks')\
+        .get_all(block_id, index='block_id')\
+        .merge({
+            'id': r.row['block_id'],
+            'num': r.row['block_num']
+        })\
+        .without('block_id', 'block_num')\
+        .coerce_to('array').run(conn)
     try:
-        result = await cursor.next()
-    except ReqlCursorEmpty:
+        return resource[0]
+    except IndexError:
         raise ApiNotFound(
             "Not Found: No block with the id '{}' exists.".format(block_id)
         )
-    return result
 
 
 async def fetch_block_by_num(conn, block_num):
-    block = await r.table('blocks').get(block_num).run(conn)
-    if block is None:
+    try:
+        return await r.table('blocks')\
+            .get(block_num)\
+            .merge({
+                'id': r.row['block_id'],
+                'num': r.row['block_num']
+            })\
+            .without('block_id', 'block_num').run(conn)
+    except ReqlRuntimeError:
         raise ApiNotFound(
             "Not Found: "
             "No block with the block num '{}' exists.".format(block_num)
         )
-    return block
