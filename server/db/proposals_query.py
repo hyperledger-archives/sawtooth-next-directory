d4bb781c122ac4a5bb10f5ab5a41710ece2d9cb8
Darian Plumb <dplumb@bitwise.io>
2017-11-27 15:04:14 -0600

    Add proposal fetch routes
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- server/db/proposals_query.py
+++ server/db/proposals_query.py
@@ -15,10 +15,80 @@
 
 import logging
 
+from api.errors import ApiNotFound
+
 import rethinkdb as r
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+async def fetch_all_proposal_resources(conn, head_block_num, start, limit):
+    return await r.table('proposals')\
+        .order_by(index='proposal_id')\
+        .filter((head_block_num >= r.row['start_block_num'])
+                & (head_block_num < r.row['end_block_num']))\
+        .slice(start, start+limit)\
+        .map(lambda proposal: proposal.merge({
+            'id': proposal['proposal_id'],
+            'type': proposal['proposal_type'],
+            'object': proposal['object_id'],
+            'target': proposal['target_id'],
+            'status': proposal['status'],
+        }))\
+        .map(lambda proposal: (proposal['metadata'] == "").branch(
+            proposal.without('metadata'), proposal
+        ))\
+        .without(
+            'start_block_num',
+            'end_block_num',
+            'proposal_id',
+            'object_id',
+            'target_id'
+        )\
+        .coerce_to('array').run(conn)
+
+
+async def fetch_proposal_resource(conn, proposal_id, head_block_num):
+    resource = await r.table('proposals')\
+        .get_all(proposal_id, index='proposal_id')\
+        .filter((head_block_num >= r.row['start_block_num'])
+                & (head_block_num < r.row['end_block_num']))\
+        .map(lambda proposal: proposal.merge({
+            'id': proposal['proposal_id'],
+            'type': proposal['proposal_type'],
+            'object': proposal['object_id'],
+            'target': proposal['target_id'],
+            'status': proposal['status'],
+        }))\
+        .map(lambda proposal: (proposal['metadata'] == "").branch(
+            proposal.without('metadata'), proposal
+        ))\
+        .without(
+            'start_block_num',
+            'end_block_num',
+            'proposal_id',
+            'object_id',
+            'target_id'
+        )\
+        .coerce_to('array').run(conn)
+    try:
+        return resource[0]
+    except IndexError:
+        raise ApiNotFound(
+            'Not Found: No proposal with the id {} exists'.format(proposal_id)
+        )
+
+
+def fetch_approver_ids(table, object_id, head_block_num):
+    return r.table(table)\
+        .get_all(object_id)\
+        .filter(lambda doc:
+                (head_block_num >= doc['start_block_num'])
+                & (head_block_num < doc['end_block_num']))\
+        .pluck('identifiers', 'manager')\
+        .coerce_to('array')\
+        .concat_map(lambda identifiers: identifiers)
 
 
 def fetch_proposal_ids_by_target(target, head_block_num):
