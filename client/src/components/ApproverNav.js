47aa1a7fddebc9c9a2f36267af9c898fd32a1913
PGobz <p.gobin@gmail.com>
2018-09-26 13:10:54 -0700

    Add base UI scaffolding
    UI scaffolding based on CRA from Facebook. Redux/Sagas with React
    Router, reduxsauce (sugar/utility), seamless-immutable (immutable
    data structures), and Semantic UI.
    
    .travis.yml has been split into two stages; one for the backend and
    one for the frontend. The frontend build is on LTS, which as of this
    commit is v8.12.
    
    Root project Dockerfile has not been modified. If you would like to
    test the new client in the context of Docker, simply switch ports
    4200 -> 3000 and directory ./ui -> ./client.
    
    Signed-off-by: PGobz <p.gobin@gmail.com>

---
+++ client/src/components/ApproverNav.js
@@ -0,0 +1,93 @@
+/* Copyright 2018 Contributors to Hyperledger Sawtooth
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+----------------------------------------------------------------------------- */
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { Container, List, Image, Search } from 'semantic-ui-react';
+
+
+import './styles/ApproverNav.css';
+
+
+/**
+ * 
+ * @class ApproverNav
+ * Component encapsulating the template for the sidebar displayed
+ * on the approver landing page.
+ * 
+ */
+export default class ApproverNav extends Component {
+
+  render () {
+    return (
+      <Container>
+        <Search className='next-approver-nav-search' category loading={false}/>
+        <h3>Pending</h3>
+
+        <List selection verticalAlign='middle'>
+          <Link to='/approval-home/batch'>
+            <List.Item>
+              <Image src=''/>
+              <List.Content>
+                <List.Header>Batch</List.Header>
+              </List.Content>
+            </List.Item>
+          </Link>
+          <Link to='/approval-home/roles'>
+            <List.Item>
+              <Image src=''/>
+              <List.Content>
+                <List.Header>Roles</List.Header>
+              </List.Content>
+            </List.Item>
+          </Link>
+          <Link to='/approval-home/individuals'>
+            <List.Item>
+              <Image src=''/>
+              <List.Content>
+                <List.Header>Individuals</List.Header>
+              </List.Content>
+            </List.Item>
+          </Link>
+          <Link to='/approval-home/frequent'>
+            <List.Item>
+              <Image src=''/>
+              <List.Content>
+                <List.Header>Frequent</List.Header>
+              </List.Content>
+            </List.Item>
+          </Link>
+          <Link to='/approval-home/expiring'>
+            <List.Item>
+              <Image src=''/>
+              <List.Content>
+                <List.Header>About to Expire</List.Header>
+              </List.Content>
+            </List.Item>
+          </Link>
+        </List>
+
+        <h3>Approved</h3>
+        <h3>Expired</h3>
+        <h3>Manage Groups</h3>
+        <h3>People</h3>
+
+        <Link to='/home' id='next-switch-approver-link'>Switch to Requester</Link>
+
+      </Container>
+    );
+  }
+
+}
---
+++ client/src/components/forms/LoginForm.js
@@ -0,0 +1,72 @@
+/* Copyright 2018 Contributors to Hyperledger Sawtooth
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+----------------------------------------------------------------------------- */
+
+
+import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
+
+
+/**
+ * 
+ * @class LoginForm
+ * Component encapsulating a reusable login form suitable for
+ * composing within containers where login functionality is required
+ * 
+ */
+export default class LoginForm extends Component {
+
+  constructor (props) {
+    super(props);
+
+    // TODO: Consider moving to Redux
+    this.state = { username: '', password: '' };
+  }
+
+
+  /**
+   * 
+   * @param event   Event passed by Semantic UI
+   * @param name    Name of form element derived from 'name' HTML attribute
+   * @param value   Value of form field
+   * 
+   */
+  handleChange = (event, { name, value }) => {
+    this.setState({ [name]: value });
+  }
+
+  
+  render () {
+    const { submit } = this.props;
+    const { username, password } = this.state;
+
+    return (
+      <Form onSubmit={() => submit(username, password)}>
+        <Form.Input
+          label='User ID'
+          placeholder='User ID'
+          name='username'
+          onChange={this.handleChange}/>
+        <Form.Input
+          label='Password'
+          placeholder='Password'
+          name='password'
+          type='password'
+          onChange={this.handleChange}/>
+        <Form.Button content='Login'/>
+      </Form>
+    );
+  }
+  
+}
