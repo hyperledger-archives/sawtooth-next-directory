50c10af254cf0e56c3e5d4662c181d07bc2ab48d
PGobz <p.gobin@gmail.com>
2018-09-26 13:43:54 -0700

    Clean up whitespace and EOF characters
    Signed-off-by: PGobz <p.gobin@gmail.com>

--- client/src/components/ApproverNav.js
+++ client/src/components/ApproverNav.js
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ----------------------------------------------------------------------------- */
 
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Container, List, Image, Search } from 'semantic-ui-react';
--- client/src/components/RequesterNav.js
+++ client/src/components/RequesterNav.js
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ----------------------------------------------------------------------------- */
 
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Button, Container, Search } from 'semantic-ui-react';
