e05a2bc8c046b39d07eec7726486ceb2c411670a
Nadia Bahrami <nadia.bahrami@dev9.com>
2018-10-12 13:47:05 -0700

    Updating all licenses
    Signed-off-by: Nadia Bahrami <nadia.bahrami@dev9.com>

--- rbac/addressing/addresser.py
+++ rbac/addressing/addresser.py
@@ -1,4 +1,4 @@
-# Copyright contributors to Hyperledger Sawtooth
+# Copyright 2018 Contributors to Hyperledger Sawtooth
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -----------------------------------------------------------------------------
-
+# ------------------------------------------------------------------------------
 import re
 import enum
 from hashlib import sha512
