bb5852d193a56b8e013190344cdd25d4cc6e4451
Adam Gering <adam.gering@dev9.com>
2018-10-23 18:20:30 -0700

    Add role proposals to role manager
    Also adds inputs/outputs to payload to simplify testing
    
    Signed-off-by: Adam Gering <adam.gering@dev9.com>

--- rbac/common/protobuf/rbac_payload_pb2.py
+++ rbac/common/protobuf/rbac_payload_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='rbac_payload.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\x12rbac_payload.proto\"\xf9\n\n\x0bRBACPayload\x12.\n\x0cmessage_type\x18\x01 \x01(\x0e\x32\x18.RBACPayload.MessageType\x12\x0f\n\x07\x63ontent\x18\x02 \x01(\x0c\"\xa8\n\n\x0bMessageType\x12\x1a\n\x16PROPOSE_ADD_ROLE_TASKS\x10\x00\x12\x1c\n\x18PROPOSE_ADD_ROLE_MEMBERS\x10\x01\x12\x1b\n\x17PROPOSE_ADD_ROLE_OWNERS\x10\x02\x12\x1b\n\x17PROPOSE_ADD_ROLE_ADMINS\x10\x03\x12\x1d\n\x19PROPOSE_REMOVE_ROLE_TASKS\x10\x04\x12\x1f\n\x1bPROPOSE_REMOVE_ROLE_MEMBERS\x10\x05\x12\x1e\n\x1aPROPOSE_REMOVE_ROLE_OWNERS\x10\x06\x12\x1e\n\x1aPROPOSE_REMOVE_ROLE_ADMINS\x10\x07\x12\x1a\n\x16\x43ONFIRM_ADD_ROLE_TASKS\x10\x08\x12\x1c\n\x18\x43ONFIRM_ADD_ROLE_MEMBERS\x10\t\x12\x1b\n\x17\x43ONFIRM_ADD_ROLE_OWNERS\x10\n\x12\x1b\n\x17\x43ONFIRM_ADD_ROLE_ADMINS\x10\x0b\x12\x1d\n\x19\x43ONFIRM_REMOVE_ROLE_TASKS\x10\x0c\x12\x1f\n\x1b\x43ONFIRM_REMOVE_ROLE_MEMBERS\x10\r\x12\x1e\n\x1a\x43ONFIRM_REMOVE_ROLE_OWNERS\x10\x0e\x12\x1e\n\x1a\x43ONFIRM_REMOVE_ROLE_ADMINS\x10\x0f\x12\x19\n\x15REJECT_ADD_ROLE_TASKS\x10\x10\x12\x1b\n\x17REJECT_ADD_ROLE_MEMBERS\x10\x11\x12\x1a\n\x16REJECT_ADD_ROLE_OWNERS\x10\x12\x12\x1a\n\x16REJECT_ADD_ROLE_ADMINS\x10\x13\x12\x1c\n\x18REJECT_REMOVE_ROLE_TASKS\x10\x14\x12\x1e\n\x1aREJECT_REMOVE_ROLE_MEMBERS\x10\x15\x12\x1d\n\x19REJECT_REMOVE_ROLE_OWNERS\x10\x16\x12\x1d\n\x19REJECT_REMOVE_ROLE_ADMINS\x10\x17\x12\x1b\n\x17PROPOSE_ADD_TASK_OWNERS\x10\x18\x12\x1b\n\x17PROPOSE_ADD_TASK_ADMINS\x10\x19\x12\x1e\n\x1aPROPOSE_REMOVE_TASK_OWNERS\x10\x1a\x12\x1e\n\x1aPROPOSE_REMOVE_TASK_ADMINS\x10\x1b\x12\x1b\n\x17\x43ONFIRM_ADD_TASK_OWNERS\x10\x1c\x12\x1b\n\x17\x43ONFIRM_ADD_TASK_ADMINS\x10\x1d\x12\x1e\n\x1a\x43ONFIRM_REMOVE_TASK_OWNERS\x10\x1e\x12\x1e\n\x1a\x43ONFIRM_REMOVE_TASK_ADMINS\x10\x1f\x12\x1a\n\x16REJECT_ADD_TASK_OWNERS\x10 \x12\x1a\n\x16REJECT_ADD_TASK_ADMINS\x10!\x12\x1d\n\x19REJECT_REMOVE_TASK_OWNERS\x10\"\x12\x1d\n\x19REJECT_REMOVE_TASK_ADMINS\x10#\x12\x1f\n\x1bPROPOSE_UPDATE_USER_MANAGER\x10$\x12\x1f\n\x1b\x43ONFIRM_UPDATE_USER_MANAGER\x10%\x12\x1e\n\x1aREJECT_UPDATE_USER_MANAGER\x10&\x12\x0f\n\x0b\x43REATE_ROLE\x10\'\x12\x0f\n\x0b\x43REATE_USER\x10(\x12\x0f\n\x0b\x43REATE_TASK\x10)\x12\x0f\n\x0bUPDATE_ROLE\x10*\x12\x13\n\x0fUPDATE_PROPOSAL\x10+\x12\x0f\n\x0bUPDATE_TASK\x10,\x12\x0f\n\x0bUPDATE_USER\x10-b\x06proto3')
+  serialized_pb=_b('\n\x12rbac_payload.proto\"\x9a\x0b\n\x0bRBACPayload\x12.\n\x0cmessage_type\x18\x01 \x01(\x0e\x32\x18.RBACPayload.MessageType\x12\x0f\n\x07\x63ontent\x18\x02 \x01(\x0c\x12\x0e\n\x06inputs\x18\x03 \x03(\t\x12\x0f\n\x07outputs\x18\x04 \x03(\t\"\xa8\n\n\x0bMessageType\x12\x1a\n\x16PROPOSE_ADD_ROLE_TASKS\x10\x00\x12\x1c\n\x18PROPOSE_ADD_ROLE_MEMBERS\x10\x01\x12\x1b\n\x17PROPOSE_ADD_ROLE_OWNERS\x10\x02\x12\x1b\n\x17PROPOSE_ADD_ROLE_ADMINS\x10\x03\x12\x1d\n\x19PROPOSE_REMOVE_ROLE_TASKS\x10\x04\x12\x1f\n\x1bPROPOSE_REMOVE_ROLE_MEMBERS\x10\x05\x12\x1e\n\x1aPROPOSE_REMOVE_ROLE_OWNERS\x10\x06\x12\x1e\n\x1aPROPOSE_REMOVE_ROLE_ADMINS\x10\x07\x12\x1a\n\x16\x43ONFIRM_ADD_ROLE_TASKS\x10\x08\x12\x1c\n\x18\x43ONFIRM_ADD_ROLE_MEMBERS\x10\t\x12\x1b\n\x17\x43ONFIRM_ADD_ROLE_OWNERS\x10\n\x12\x1b\n\x17\x43ONFIRM_ADD_ROLE_ADMINS\x10\x0b\x12\x1d\n\x19\x43ONFIRM_REMOVE_ROLE_TASKS\x10\x0c\x12\x1f\n\x1b\x43ONFIRM_REMOVE_ROLE_MEMBERS\x10\r\x12\x1e\n\x1a\x43ONFIRM_REMOVE_ROLE_OWNERS\x10\x0e\x12\x1e\n\x1a\x43ONFIRM_REMOVE_ROLE_ADMINS\x10\x0f\x12\x19\n\x15REJECT_ADD_ROLE_TASKS\x10\x10\x12\x1b\n\x17REJECT_ADD_ROLE_MEMBERS\x10\x11\x12\x1a\n\x16REJECT_ADD_ROLE_OWNERS\x10\x12\x12\x1a\n\x16REJECT_ADD_ROLE_ADMINS\x10\x13\x12\x1c\n\x18REJECT_REMOVE_ROLE_TASKS\x10\x14\x12\x1e\n\x1aREJECT_REMOVE_ROLE_MEMBERS\x10\x15\x12\x1d\n\x19REJECT_REMOVE_ROLE_OWNERS\x10\x16\x12\x1d\n\x19REJECT_REMOVE_ROLE_ADMINS\x10\x17\x12\x1b\n\x17PROPOSE_ADD_TASK_OWNERS\x10\x18\x12\x1b\n\x17PROPOSE_ADD_TASK_ADMINS\x10\x19\x12\x1e\n\x1aPROPOSE_REMOVE_TASK_OWNERS\x10\x1a\x12\x1e\n\x1aPROPOSE_REMOVE_TASK_ADMINS\x10\x1b\x12\x1b\n\x17\x43ONFIRM_ADD_TASK_OWNERS\x10\x1c\x12\x1b\n\x17\x43ONFIRM_ADD_TASK_ADMINS\x10\x1d\x12\x1e\n\x1a\x43ONFIRM_REMOVE_TASK_OWNERS\x10\x1e\x12\x1e\n\x1a\x43ONFIRM_REMOVE_TASK_ADMINS\x10\x1f\x12\x1a\n\x16REJECT_ADD_TASK_OWNERS\x10 \x12\x1a\n\x16REJECT_ADD_TASK_ADMINS\x10!\x12\x1d\n\x19REJECT_REMOVE_TASK_OWNERS\x10\"\x12\x1d\n\x19REJECT_REMOVE_TASK_ADMINS\x10#\x12\x1f\n\x1bPROPOSE_UPDATE_USER_MANAGER\x10$\x12\x1f\n\x1b\x43ONFIRM_UPDATE_USER_MANAGER\x10%\x12\x1e\n\x1aREJECT_UPDATE_USER_MANAGER\x10&\x12\x0f\n\x0b\x43REATE_ROLE\x10\'\x12\x0f\n\x0b\x43REATE_USER\x10(\x12\x0f\n\x0b\x43REATE_TASK\x10)\x12\x0f\n\x0bUPDATE_ROLE\x10*\x12\x13\n\x0fUPDATE_PROPOSAL\x10+\x12\x0f\n\x0bUPDATE_TASK\x10,\x12\x0f\n\x0bUPDATE_USER\x10-b\x06proto3')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -218,8 +218,8 @@ _RBACPAYLOAD_MESSAGETYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=104,
-  serialized_end=1424,
+  serialized_start=137,
+  serialized_end=1457,
 )
 _sym_db.RegisterEnumDescriptor(_RBACPAYLOAD_MESSAGETYPE)
 
@@ -245,6 +245,20 @@ _RBACPAYLOAD = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='inputs', full_name='RBACPayload.inputs', index=2,
+      number=3, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='outputs', full_name='RBACPayload.outputs', index=3,
+      number=4, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -259,7 +273,7 @@ _RBACPAYLOAD = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=23,
-  serialized_end=1424,
+  serialized_end=1457,
 )
 
 _RBACPAYLOAD.fields_by_name['message_type'].enum_type = _RBACPAYLOAD_MESSAGETYPE
--- rbac/common/user/user_manager.py
+++ rbac/common/user/user_manager.py
@@ -15,31 +15,29 @@
 
 import logging
 from rbac.common.crypto.keys import Key
-from rbac.addressing.addresser import make_user_address
-from rbac.common.sawtooth.client_sync import ClientSync
+from rbac.common.user.user_address import make_user_address
 from rbac.common.protobuf.rbac_payload_pb2 import RBACPayload
 from rbac.common.protobuf import user_transaction_pb2
 from rbac.common.protobuf import user_state_pb2
-from rbac.common.sawtooth.batcher import Batcher
+from rbac.common.manager.base_manager import BaseManager
 
 LOGGER = logging.getLogger(__name__)
 
 
-class UserManager:
+class UserManager(BaseManager):
     def __init__(self):
-        self.batch = Batcher()
-        self.client = ClientSync()
+        BaseManager.__init__(self)
+
+    def address(self, user_id):
+        """Make an address for the given user_id"""
+        return make_user_address(user_id=user_id)
 
     def make(
         self, user_id, name, user_name=None, email=None, metadata=None, manager_id=None
     ):
-        return user_state_pb2.User(
-            user_id=user_id,
-            name=name,
-            # user_name=user_name,
-            # email=email,
-            metadata=metadata,
-            manager_id=manager_id,
+        """Makes a CreateUser message"""
+        return user_transaction_pb2.CreateUser(
+            user_id=user_id, name=name, metadata=metadata, manager_id=manager_id
         )
 
     def make_with_key(
@@ -51,10 +49,11 @@ class UserManager:
         metadata=None,
         manager_id=None,
     ):
+        """Makes a CreateUser message with a new keypair"""
         keypair = Key()
         if user_id is None:
             user_id = keypair.public_key
-        user = self.make(
+        message = self.make(
             user_id=user_id,
             name=name,
             user_name=user_name,
@@ -62,50 +61,49 @@ class UserManager:
             metadata=metadata,
             manager_id=manager_id,
         )
-        return user, keypair
+        return message, keypair
 
-    def create(self, signer_keypair, user, do_batch=True, do_send=True, do_get=False):
+    def make_payload(self, message):
+        """Make a payload for the given message type"""
+        if isinstance(message, user_transaction_pb2.CreateUser):
+            message_type = RBACPayload.CREATE_USER
+            inputs, outputs = self.make_addresses(message=message)
+        else:
+            raise TypeError(
+                "Expected message to be a user_transaction_pb2.CreateUser, use make first"
+            )
+
+        return self.batch.make_payload(
+            message=message, message_type=message_type, inputs=inputs, outputs=outputs
+        )
+
+    def make_addresses(self, message):
+        """Makes the approporiate inputs & output addresses for the message type"""
+        if isinstance(message, user_transaction_pb2.CreateUser):
+            inputs = [self.address(user_id=message.user_id)]
+            if message.manager_id:
+                inputs.append(self.address(user_id=message.manager_id))
+            outputs = inputs
+        else:
+            raise TypeError("Expected message to be a user_transaction_pb2.CreateUser")
+        return inputs, outputs
+
+    def create(self, signer_keypair, message, do_send=True, do_get=False):
+        """Create a user object in the blockchain"""
         if not isinstance(signer_keypair, Key):
             raise TypeError("Expected signer_keypair to be a Key")
-        if not isinstance(user, user_state_pb2.User):
-            raise TypeError("Expected user to be a user_state_pb2.User, use make first")
 
-        message = user_transaction_pb2.CreateUser(
-            user_id=user.user_id,
-            # user_name=user_name,
-            name=user.name,
-            metadata=user.metadata,
-        )
-        inputs = [make_user_address(user_id=user.user_id)]
-
-        if user.manager_id:
-            message.manager_id = user.manager_id
-            inputs.append(make_user_address(user_id=user.manager_id))
-
-        transaction = self.batch.make_transaction(
-            message=message,
-            message_type=RBACPayload.CREATE_USER,
-            inputs=inputs,
-            outputs=inputs,
+        return self.send(
             signer_keypair=signer_keypair,
+            object_id=message.user_id,
+            payload=self.make_payload(message=message),
+            do_send=do_send,
+            do_get=do_get,
         )
 
-        if not do_batch:
-            return transaction
-
-        batch = self.batch.make_batch(transaction=transaction)
-        if not do_send:
-            return batch
-
-        batch_list = self.batch.batch_to_list(batch)
-        status = self.client.send_batches_get_status(batch_list=batch_list)
-        if not do_get:
-            return status
-
-        return self.get(user_id=user.user_id)
-
-    def get(self, user_id):
-        address = make_user_address(user_id=user_id)
+    def get(self, object_id):
+        """Gets a user object from the blockchain"""
+        address = self.address(user_id=object_id)
         user_container = user_state_pb2.UserContainer()
         user_container.ParseFromString(self.client.get_address(address=address))
         users = list(user_container.users)
@@ -115,10 +113,10 @@ class UserManager:
             LOGGER.warning(
                 "user container at address %s has more than one record, looking for %s",
                 address,
-                user_id,
+                object_id,
             )
         for user in users:
-            if user.user_id == user_id:
+            if user.user_id == object_id:
                 return user
-        LOGGER.warning("user %s not found in container address %s", user_id, address)
+        LOGGER.warning("user %s not found in container address %s", object_id, address)
         return None
