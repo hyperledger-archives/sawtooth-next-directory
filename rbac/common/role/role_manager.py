8582af3a5a307510553746bbecb612794d745cce
Adam Gering <adam.gering@dev9.com>
2018-10-23 18:20:30 -0700

    Add role proposals to role manager
    Also adds inputs/outputs to payload to simplify testing
    
    Signed-off-by: Adam Gering <adam.gering@dev9.com>

--- rbac/common/role/role_manager.py
+++ rbac/common/role/role_manager.py
@@ -14,79 +14,153 @@
 # -----------------------------------------------------------------------------
 
 import logging
+from uuid import uuid4
 from rbac.common.crypto.keys import Key
 from rbac.addressing import addresser
 from rbac.common.protobuf.rbac_payload_pb2 import RBACPayload
 from rbac.common.protobuf import role_transaction_pb2
 from rbac.common.protobuf import role_state_pb2
-from rbac.common.sawtooth.client_sync import ClientSync
-from rbac.common.sawtooth.batcher import Batcher
+from rbac.common.manager.base_manager import BaseManager
 
 LOGGER = logging.getLogger(__name__)
 
 
-class RoleManager:
+class RoleManager(BaseManager):
     def __init__(self):
-        self.batch = Batcher()
-        self.client = ClientSync()
+        BaseManager.__init__(self)
 
-    def make(self, role_id, name, metadata=None, admins=None, owners=None):
+    def make(self, role_id, name, metadata=None, owners=None, admins=None):
+        """Make a CreateRole message"""
         return role_transaction_pb2.CreateRole(
-            role_id=role_id, name=name, metadata=metadata, admins=admins, owners=owners
+            role_id=role_id, name=name, metadata=metadata, owners=owners, admins=admins
         )
 
-    def create(self, signer_keypair, role, do_batch=True, do_send=True, do_get=False):
-        if not isinstance(signer_keypair, Key):
-            raise TypeError("Expected signer_keypair to be a Key")
-        if not isinstance(role, role_transaction_pb2.CreateRole):
+    def make_proposal(
+        self, message_proto, role_id, user_id, reason=None, metadata=None
+    ):
+        """Make a role proposal message"""
+        return message_proto(
+            proposal_id=uuid4().hex,
+            role_id=role_id,
+            user_id=user_id,
+            reason=reason,
+            metadata=metadata,
+        )
+
+    def make_payload(self, message, signer_public_key):
+        """Make a payload for the given message type"""
+        if isinstance(message, role_transaction_pb2.CreateRole):
+            message_type = RBACPayload.CREATE_ROLE
+        elif isinstance(message, role_transaction_pb2.ProposeAddRoleMember):
+            message_type = RBACPayload.PROPOSE_ADD_ROLE_MEMBERS
+        elif isinstance(message, role_transaction_pb2.ProposeAddRoleOwner):
+            message_type = RBACPayload.PROPOSE_ADD_ROLE_OWNERS
+        elif isinstance(message, role_transaction_pb2.ProposeAddRoleAdmin):
+            message_type = RBACPayload.PROPOSE_ADD_ROLE_ADMINS
+        else:
             raise TypeError(
-                "Expected role to be a role_transaction_pb2.CreateRole, use make first"
+                "RoleManager.make_payload doesn't support message type {}".format(
+                    type(message)
+                )
             )
 
-        inputs = [
-            addresser.make_sysadmin_members_address(signer_keypair.public_key),
-            addresser.make_role_attributes_address(role.role_id),
-        ]
-        inputs.extend([addresser.make_user_address(u) for u in role.admins])
-        inputs.extend([addresser.make_user_address(u) for u in role.owners])
-        inputs.extend(
-            [
-                addresser.make_role_admins_address(role_id=role.role_id, user_id=a)
-                for a in role.admins
-            ]
+        inputs, outputs = self.make_addresses(
+            message=message, signer_public_key=signer_public_key
         )
-        inputs.extend(
-            [
-                addresser.make_role_owners_address(role_id=role.role_id, user_id=o)
-                for o in role.owners
-            ]
+        return self.batch.make_payload(
+            message=message, message_type=message_type, inputs=inputs, outputs=outputs
         )
 
-        transaction = self.batch.make_transaction(
-            message=role,
-            message_type=RBACPayload.CREATE_ROLE,
-            inputs=inputs,
-            outputs=inputs,
+    def make_addresses(self, message, signer_public_key=None):
+        """Makes the approporiate inputs & output addresses for the message type"""
+        if isinstance(message, role_transaction_pb2.CreateRole):
+            inputs = [
+                # addresser.make_sysadmin_members_address(signer_public_key),
+                addresser.make_role_attributes_address(message.role_id)
+            ]
+            inputs.extend([addresser.make_user_address(u) for u in message.admins])
+            inputs.extend([addresser.make_user_address(u) for u in message.owners])
+            inputs.extend(
+                [
+                    addresser.make_role_admins_address(
+                        role_id=message.role_id, user_id=a
+                    )
+                    for a in message.admins
+                ]
+            )
+            inputs.extend(
+                [
+                    addresser.make_role_owners_address(
+                        role_id=message.role_id, user_id=o
+                    )
+                    for o in message.owners
+                ]
+            )
+            outputs = inputs
+
+        elif isinstance(message, role_transaction_pb2.ProposeAddRoleMember):
+            relationship_address = addresser.make_role_members_address(
+                role_id=message.role_id, user_id=message.user_id
+            )
+        elif isinstance(message, role_transaction_pb2.ProposeAddRoleOwner):
+            relationship_address = addresser.make_role_owners_address(
+                role_id=message.role_id, user_id=message.user_id
+            )
+        elif isinstance(message, role_transaction_pb2.ProposeAddRoleAdmin):
+            relationship_address = addresser.make_role_admins_address(
+                role_id=message.role_id, user_id=message.user_id
+            )
+        else:
+            raise TypeError(
+                "RoleManager.make_addresses doesn't support message type {}".format(
+                    type(message)
+                )
+            )
+
+        if (
+            isinstance(message, role_transaction_pb2.ProposeAddRoleMember)
+            or isinstance(message, role_transaction_pb2.ProposeAddRoleOwner)
+            or isinstance(message, role_transaction_pb2.ProposeAddRoleAdmin)
+        ):
+
+            proposal_address = addresser.make_proposal_address(
+                object_id=message.role_id, related_id=message.user_id
+            )
+
+            role_address = addresser.make_role_attributes_address(
+                role_id=message.role_id
+            )
+            user_address = addresser.make_user_address(user_id=message.user_id)
+
+            inputs = [
+                relationship_address,
+                role_address,
+                user_address,
+                proposal_address,
+            ]
+            outputs = [proposal_address]
+
+        return inputs, outputs
+
+    def create(self, signer_keypair, message, do_send=True, do_get=False):
+        """Execute the given message on the blockchain"""
+        if not isinstance(signer_keypair, Key):
+            raise TypeError("Expected signer_keypair to be a Key")
+
+        return self.send(
             signer_keypair=signer_keypair,
+            object_id=message.role_id,
+            payload=self.make_payload(
+                message=message, signer_public_key=signer_keypair.public_key
+            ),
+            do_send=do_send,
+            do_get=do_get,
         )
 
-        if not do_batch:
-            return transaction
-
-        batch = self.batch.make_batch(transaction=transaction)
-        if not do_send:
-            return batch
-
-        batch_list = self.batch.batch_to_list(batch)
-        status = self.client.send_batches_get_status(batch_list=batch_list)
-        if not do_get:
-            return status
-
-        return self.get(role_id=role.role_id)
-
-    def get(self, role_id):
+    def get(self, object_id):
+        """Get the given role object on the blockchain"""
         container = role_state_pb2.RoleAttributesContainer()
-        address = addresser.make_role_attributes_address(role_id=role_id)
+        address = addresser.make_role_attributes_address(role_id=object_id)
         container.ParseFromString(self.client.get_address(address=address))
         items = list(container.role_attributes)
         if len(items) == 0:
@@ -94,21 +168,22 @@ class RoleManager:
         elif len(items) > 1:
             LOGGER.warning(
                 "role container for %s at address %s has more than one role record",
-                role_id,
+                object_id,
                 address,
             )
         return items[0]
 
-    def check_owner(self, role_id, user_id):
+    def check_relationship(self, address_function, role_id, user_id):
+        """Check the existence of a relationship record"""
         container = role_state_pb2.RoleRelationshipContainer()
-        address = addresser.make_role_owners_address(role_id=role_id, user_id=user_id)
+        address = address_function(role_id=role_id, user_id=user_id)
         container.ParseFromString(self.client.get_address(address=address))
         items = list(container.relationships)
         if len(items) == 0:
             return False
         elif len(items) > 1:
             LOGGER.warning(
-                "role %s owners container for user %s at address %s has more than one record",
+                "role %s relationship container for user %s at address %s has more than one record",
                 role_id,
                 user_id,
                 address,
@@ -117,7 +192,7 @@ class RoleManager:
         identifiers = list(item.identifiers)
         if len(identifiers) == 0:
             LOGGER.warning(
-                "role %s owners container for user %s at address %s has no identifiers",
+                "role %s relationship container for user %s at address %s has no identifiers",
                 role_id,
                 user_id,
                 address,
@@ -125,42 +200,63 @@ class RoleManager:
             return False
         if len(identifiers) > 1:
             LOGGER.warning(
-                "role %s owners container for user %s at address %s has more than one identifier",
+                "role %s relationship container for user %s at address %s has more than one identifier",
                 role_id,
                 user_id,
                 address,
             )
         return bool(user_id in item.identifiers)
 
+    def check_member(self, role_id, user_id):
+        """Check if a user is a member of a role"""
+        return self.check_relationship(
+            address_function=addresser.make_role_members_address,
+            role_id=role_id,
+            user_id=user_id,
+        )
+
+    def check_owner(self, role_id, user_id):
+        """Check if a user is an owner of a role"""
+        return self.check_relationship(
+            address_function=addresser.make_role_owners_address,
+            role_id=role_id,
+            user_id=user_id,
+        )
+
     def check_admin(self, role_id, user_id):
-        container = role_state_pb2.RoleRelationshipContainer()
-        address = addresser.make_role_admins_address(role_id=role_id, user_id=user_id)
-        container.ParseFromString(self.client.get_address(address=address))
-        items = list(container.relationships)
-        if len(items) == 0:
-            return False
-        elif len(items) > 1:
-            LOGGER.warning(
-                "role %s admins container for user %s at address %s has more than one record",
-                role_id,
-                user_id,
-                address,
-            )
-        item = items[0]
-        identifiers = list(item.identifiers)
-        if len(identifiers) == 0:
-            LOGGER.warning(
-                "role %s admins container for user %s at address %s has no identifiers",
-                role_id,
-                user_id,
-                address,
-            )
-            return False
-        if len(identifiers) > 1:
-            LOGGER.warning(
-                "role %s admins container for user %s at address %s has more than one identifier",
-                role_id,
-                user_id,
-                address,
-            )
-        return bool(user_id in item.identifiers)
+        """Check if a user is an admin of a role"""
+        return self.check_relationship(
+            address_function=addresser.make_role_admins_address,
+            role_id=role_id,
+            user_id=user_id,
+        )
+
+    def make_propose_member(self, role_id, user_id, reason=None, metadata=None):
+        """Make a proposal message that a user be added to a role"""
+        return self.make_proposal(
+            message_proto=role_transaction_pb2.ProposeAddRoleMember,
+            role_id=role_id,
+            user_id=user_id,
+            reason=reason,
+            metadata=metadata,
+        )
+
+    def make_propose_owner(self, role_id, user_id, reason=None, metadata=None):
+        """Make a proposal message that a user own a role"""
+        return self.make_proposal(
+            message_proto=role_transaction_pb2.ProposeAddRoleOwner,
+            role_id=role_id,
+            user_id=user_id,
+            reason=reason,
+            metadata=metadata,
+        )
+
+    def make_propose_admin(self, role_id, user_id, reason=None, metadata=None):
+        """Make a proposal message that a user admin a role"""
+        return self.make_proposal(
+            message_proto=role_transaction_pb2.ProposeAddRoleAdmin,
+            role_id=role_id,
+            user_id=user_id,
+            reason=reason,
+            metadata=metadata,
+        )
--- tests/rbac/common/role/role_manager_test.py
+++ tests/rbac/common/role/role_manager_test.py
@@ -19,31 +19,33 @@ import random
 from uuid import uuid4
 
 from rbac.common.crypto.keys import Key
+from rbac.common.protobuf.rbac_payload_pb2 import RBACPayload
 from rbac.common.protobuf import user_state_pb2
+from rbac.common.protobuf import user_transaction_pb2
 from tests.rbac.common.user.user_manager_test import UserManagerTest
 from rbac.common.role.role_manager import RoleManager
-
-# from rbac.common.proposal.proposal_manager import ProposalManager
-# from tests.rbac.common.user.test_user_manager import TestUserManager
 from rbac.common.protobuf import role_transaction_pb2
-from tests.rbac.common.manager.test_assertions import TestAssertions
+from tests.rbac.common.role.role_assertions import RoleAssertions
 
 LOGGER = logging.getLogger(__name__)
 
 
 @pytest.mark.role
 @pytest.mark.role_it
-class RoleManagerTest(RoleManager, TestAssertions):
+class RoleManagerTest(RoleManager, RoleAssertions):
     def __init__(self, *args, **kwargs):
-        TestAssertions.__init__(self, *args, **kwargs)
+        """Test the RoleManager class"""
+        RoleAssertions.__init__(self, *args, **kwargs)
         RoleManager.__init__(self)
         self.user = UserManagerTest()
 
-    def get_data_user_role(self):
-        user, keypair = self.user.test_create()
-        self.assertIsInstance(user, user_state_pb2.User)
+    def get_testunit_user_role(self):
+        """Get a test data for a role, user and key with the user
+        as the role owner and admin. User has not been created."""
+        user, keypair = self.user.get_testdata_user_with_key()
+        self.assertIsInstance(user, user_transaction_pb2.CreateUser)
         self.assertIsInstance(keypair, Key)
-        name = self.get_data_rolename()
+        name = self.get_testdata_rolename()
         role_id = uuid4().hex
         role = self.make(
             role_id=role_id, name=name, admins=[user.user_id], owners=[user.user_id]
@@ -53,8 +55,25 @@ class RoleManagerTest(RoleManager, TestAssertions):
         self.assertEqual(role.name, name)
         return role, user, keypair
 
-    def get_data_role(self, role_id=None):
-        name = self.get_data_rolename()
+    def get_testdata_user_role(self):
+        """Get a test data for a role, user and key with the user
+        as the role owner and admin. User has been created."""
+        user, keypair = self.user.test_create()
+        self.assertIsInstance(user, user_state_pb2.User)
+        self.assertIsInstance(keypair, Key)
+        name = self.get_testdata_rolename()
+        role_id = uuid4().hex
+        role = self.make(
+            role_id=role_id, name=name, admins=[user.user_id], owners=[user.user_id]
+        )
+        self.assertIsInstance(role, role_transaction_pb2.CreateRole)
+        self.assertEqual(role.role_id, role_id)
+        self.assertEqual(role.name, name)
+        return role, user, keypair
+
+    def get_testdata_role(self, role_id=None):
+        """Get a test data for a role"""
+        name = self.get_testdata_rolename()
         if role_id is None:
             role_id = uuid4().hex
         role = self.make(role_id=role_id, name=name)
@@ -63,11 +82,17 @@ class RoleManagerTest(RoleManager, TestAssertions):
         self.assertEqual(role.name, name)
         return role
 
-    def get_data_rolename(self):
+    def get_testdata_rolename(self):
+        """Get a random name for a role"""
         return "Role" + str(random.randint(1000, 10000))
+
+    def get_testdata_reason(self):
+        """Get a random reason"""
+        return "Because" + str(random.randint(10000, 100000))
 
     @pytest.mark.unit
     def test_role_manager_interface(self):
+        """Check the expected RoleManager methods exist"""
         self.assertIsInstance(self, RoleManagerTest)
         self.assertTrue(callable(self.make))
         self.assertTrue(callable(self.create))
@@ -75,11 +100,13 @@ class RoleManagerTest(RoleManager, TestAssertions):
 
     @pytest.mark.unit
     def test_role_manager_test_interface(self):
-        self.assertTrue(callable(self.get_data_rolename))
-        self.assertTrue(callable(self.get_data_role))
+        """Check the expected RoleManager test methods exist"""
+        self.assertTrue(callable(self.get_testdata_rolename))
+        self.assertTrue(callable(self.get_testdata_role))
 
     @pytest.mark.unit
     def test_role_manager_user_interface(self):
+        """Check the expected UserManager methods exist"""
         self.assertIsInstance(self.user, UserManagerTest)
         self.assertTrue(callable(self.user.make))
         self.assertTrue(callable(self.user.create))
@@ -87,33 +114,196 @@ class RoleManagerTest(RoleManager, TestAssertions):
 
     @pytest.mark.unit
     def test_role_manager_user_test_interface(self):
+        """Check the expected UserManager test methods exist"""
         self.assertIsInstance(self.user, UserManagerTest)
-        self.assertTrue(callable(self.user.get_test_name))
-        self.assertTrue(callable(self.user.get_test_username))
-        self.assertTrue(callable(self.user.get_test_user))
-        self.assertTrue(callable(self.user.get_test_user_with_key))
+        self.assertTrue(callable(self.user.get_testdata_name))
+        self.assertTrue(callable(self.user.get_testdata_username))
+        self.assertTrue(callable(self.user.get_testdata_user))
+        self.assertTrue(callable(self.user.get_testdata_user_with_key))
 
     @pytest.mark.unit
-    def test_get_data_role(self):
-        role = self.get_data_role()
+    def test_get_testdata_role(self):
+        """Test getting a test data CreateRole message"""
+        role = self.get_testdata_role()
         self.assertIsInstance(role, role_transaction_pb2.CreateRole)
+
+    @pytest.mark.unit
+    def test_make_addresses(self):
+        """Test the make addresses method for a CreateRole message"""
+        self.assertTrue(callable(self.user.get_testdata_user))
+        self.assertTrue(callable(self.get_testdata_role))
+        self.assertTrue(callable(self.make_addresses))
+        _, keypair = self.user.get_testdata_user_with_key()
+        message = self.get_testdata_role()
+        inputs, outputs = self.make_addresses(
+            message=message, signer_public_key=keypair.public_key
+        )
+        self.assertIsInstance(inputs, list)
+        self.assertIsInstance(outputs, list)
+        self.assertEqual(inputs, outputs)
+
+    @pytest.mark.unit
+    def test_make_payload(self):
+        """Test making a payload for a CreateRole message"""
+        self.assertTrue(callable(self.user.get_testdata_user))
+        self.assertTrue(callable(self.get_testdata_role))
+        self.assertTrue(callable(self.make_payload))
+        _, keypair = self.user.get_testdata_user_with_key()
+        message = self.get_testdata_role()
+        payload = self.make_payload(
+            message=message, signer_public_key=keypair.public_key
+        )
+        self.assertValidPayload(
+            payload=payload, message=message, message_type=RBACPayload.CREATE_ROLE
+        )
 
     @pytest.mark.integration
     def test_create(self, role=None, user=None, keypair=None):
+        """Test creating a role"""
         if user is None and role is None:
-            role, user, keypair = self.get_data_user_role()
+            role, user, keypair = self.get_testdata_user_role()
         if user is None:
             user, keypair = self.user.test_create()
         if role is None:
-            role = self.get_data_role()
+            role = self.get_testdata_role()
             role.admins = [user.user_id]
             role.owners = [user.user_id]
         self.assertIsInstance(role, role_transaction_pb2.CreateRole)
 
-        status = self.create(signer_keypair=keypair, role=role)
-        self.assertEqual(status[0]["status"], "COMMITTED")
-        check = self.get(role_id=role.role_id)
-        self.assertEqual(check.name, role.name)
+        got, status, transaction, _, _, _ = self.create(
+            signer_keypair=keypair, message=role, do_get=True
+        )
+        self.assertValidTransaction(
+            transaction=transaction,
+            payload=self.make_payload(
+                message=role, signer_public_key=keypair.public_key
+            ),
+            signer_public_key=keypair.public_key,
+        )
+        self.assertStatusSuccess(status)
+        self.assertCreateRoleResult(role, got)
         self.assertTrue(self.check_owner(role_id=role.role_id, user_id=user.user_id))
         self.assertTrue(self.check_admin(role_id=role.role_id, user_id=user.user_id))
-        return role, user, keypair
+        return got, user, keypair
+
+    @pytest.mark.unit
+    def test_make_propose_member(self, role=None, user=None, keypair=None):
+        """Test a message proposing a member of a role"""
+        role, user, keypair = self.get_testunit_user_role()
+        message = self.make_propose_member(
+            role_id=role.role_id,
+            user_id=user.user_id,
+            reason=self.get_testdata_reason(),
+        )
+        self.assertIsInstance(message, role_transaction_pb2.ProposeAddRoleMember)
+        self.assertEqual(message.role_id, role.role_id)
+        self.assertEqual(message.user_id, user.user_id)
+
+    @pytest.mark.unit
+    def test_make_propose_owner(self, role=None, user=None, keypair=None):
+        """Test a message proposing an owner of a role"""
+        role, user, keypair = self.get_testunit_user_role()
+        message = self.make_propose_owner(
+            role_id=role.role_id,
+            user_id=user.user_id,
+            reason=self.get_testdata_reason(),
+        )
+        self.assertIsInstance(message, role_transaction_pb2.ProposeAddRoleOwner)
+        self.assertEqual(message.role_id, role.role_id)
+        self.assertEqual(message.user_id, user.user_id)
+
+    @pytest.mark.unit
+    def test_make_propose_admin(self, role=None, user=None, keypair=None):
+        """Test message proposing an admin of a role"""
+        role, user, keypair = self.get_testunit_user_role()
+        message = self.make_propose_admin(
+            role_id=role.role_id,
+            user_id=user.user_id,
+            reason=self.get_testdata_reason(),
+        )
+        self.assertIsInstance(message, role_transaction_pb2.ProposeAddRoleAdmin)
+        self.assertEqual(message.role_id, role.role_id)
+        self.assertEqual(message.user_id, user.user_id)
+
+    @pytest.mark.integration
+    def test_propose_member(self, role=None, user=None, keypair=None):
+        """Test a proposing a member of a role"""
+        role, user, keypair = self.test_create(role=role, user=user, keypair=keypair)
+        message = self.make_propose_member(
+            role_id=role.role_id,
+            user_id=user.user_id,
+            reason=self.get_testdata_reason(),
+        )
+        self.assertIsInstance(message, role_transaction_pb2.ProposeAddRoleMember)
+        self.assertEqual(message.role_id, role.role_id)
+        self.assertEqual(message.user_id, user.user_id)
+
+        got, status, transaction, _, _, _ = self.create(
+            signer_keypair=keypair, message=message, do_get=False
+        )
+        self.assertValidTransaction(
+            transaction=transaction,
+            payload=self.make_payload(
+                message=message, signer_public_key=keypair.public_key
+            ),
+            signer_public_key=keypair.public_key,
+        )
+        self.assertStatusSuccess(status)
+        # self.assertProposeMemberResult(role, got)
+        return got, user, keypair
+
+    @pytest.mark.integration
+    def test_propose_owner(self, role=None, user=None, keypair=None):
+        """Test a proposing an owner of a role"""
+        role, _, _ = self.test_create(role=role, user=user, keypair=keypair)
+        user, keypair = self.user.test_create()
+        message = self.make_propose_owner(
+            role_id=role.role_id,
+            user_id=user.user_id,
+            reason=self.get_testdata_reason(),
+        )
+        self.assertIsInstance(message, role_transaction_pb2.ProposeAddRoleOwner)
+        self.assertEqual(message.role_id, role.role_id)
+        self.assertEqual(message.user_id, user.user_id)
+
+        got, status, transaction, _, _, _ = self.create(
+            signer_keypair=keypair, message=message, do_get=False
+        )
+        self.assertValidTransaction(
+            transaction=transaction,
+            payload=self.make_payload(
+                message=message, signer_public_key=keypair.public_key
+            ),
+            signer_public_key=keypair.public_key,
+        )
+        self.assertStatusSuccess(status)
+        # self.assertProposeOwnerResult(role, got)
+        return got, user, keypair
+
+    @pytest.mark.integration
+    def test_propose_admin(self, role=None, user=None, keypair=None):
+        """Test a proposing an admin of a role"""
+        role, _, _ = self.test_create(role=role, user=user, keypair=keypair)
+        user, keypair = self.user.test_create()
+        message = self.make_propose_admin(
+            role_id=role.role_id,
+            user_id=user.user_id,
+            reason=self.get_testdata_reason(),
+        )
+        self.assertIsInstance(message, role_transaction_pb2.ProposeAddRoleAdmin)
+        self.assertEqual(message.role_id, role.role_id)
+        self.assertEqual(message.user_id, user.user_id)
+
+        got, status, transaction, _, _, _ = self.create(
+            signer_keypair=keypair, message=message, do_get=False
+        )
+        self.assertValidTransaction(
+            transaction=transaction,
+            payload=self.make_payload(
+                message=message, signer_public_key=keypair.public_key
+            ),
+            signer_public_key=keypair.public_key,
+        )
+        self.assertStatusSuccess(status)
+        # self.assertProposeAdminResult(role, got)
+        return got, user, keypair
