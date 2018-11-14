8f78e586b10c0fe27574944cd1ca4d8648fd60c2
Nadia Bahrami <nadia.bahrami@dev9.com>
2018-11-14 12:35:41 -0800

    Initial inbound sync time recorded for AAD
    Signed-off-by: Nadia Bahrami <nadia.bahrami@dev9.com>

--- rbac/providers/common/outbound_filters.py
+++ rbac/providers/common/outbound_filters.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ------------------------------------------------------------------------------
 
-from rbac.providers.provider_transforms import GROUP_TRANSFORM, USER_TRANSFORM
+from rbac.providers.common.provider_transforms import GROUP_TRANSFORM, USER_TRANSFORM
 
 
 def outbound_user_filter(sawtooth_user, provider):
