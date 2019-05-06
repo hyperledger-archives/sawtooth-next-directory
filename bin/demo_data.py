c6d0682293d0af83255a6b1eddd9e2fbefcca7c9
Nadia Bahrami <nadia.bahrami@dev9.com>
2019-05-06 15:26:31 -0700

    Seperating out the Admin defaults to new file.
    Signed-off-by: Nadia Bahrami <nadia.bahrami@dev9.com>

--- bin/demo_data.py
+++ bin/demo_data.py
@@ -14,10 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
-import os
 import logging
-import sys
-import json
 import requests
 
 LOGGER = logging.getLogger(__name__)
