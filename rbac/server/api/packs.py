166c46a37009274c7ff30e99a7d423c7343218aa
Michael Nguyen <michael.nguyen79@t-mobile.com>
2019-08-01 23:30:41 -0700

    Added swagger for Packs API endpoints
    Signed-off-by: Michael Nguyen <michael.nguyen79@t-mobile.com>

--- rbac/server/api/packs.py
+++ rbac/server/api/packs.py
@@ -17,10 +17,11 @@ from uuid import uuid4
 
 from sanic import Blueprint
 from sanic.response import json
+from sanic_openapi import doc
 
 from rbac.common.logs import get_default_logger
 from rbac.server.api.auth import authorized
-from rbac.server.api.errors import ApiBadRequest
+from rbac.server.api.errors import ApiBadRequest, ApiForbidden
 from rbac.server.api.roles import add_role_member
 from rbac.server.api.utils import (
     check_admin_status,
@@ -40,6 +41,42 @@ LOGGER = get_default_logger(__name__)
 
 
 @PACKS_BP.get("api/packs")
+@doc.summary("API Endpoint to get all packs")
+@doc.description("API Endpoint to get all packs.")
+@doc.consumes({"head": str}, location="query")
+@doc.consumes({"limit": int}, location="query")
+@doc.consumes({"start": int}, location="query")
+@doc.produces(
+    {
+        "data": [
+            {
+                "description": str,
+                "id": str,
+                "name": str,
+                "roles": [str],
+                "owners": [str],
+            }
+        ],
+        "head": str,
+        "link": str,
+        "paging": {
+            "first": str,
+            "last": str,
+            "limit": int,
+            "next": str,
+            "prev": str,
+            "start": int,
+            "total": int,
+        },
+    },
+    description="Successfully gets all packs",
+    content_type="application/json",
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
 @authorized()
 async def get_all_packs(request):
     """Get all packs"""
@@ -56,6 +93,30 @@ async def get_all_packs(request):
 
 
 @PACKS_BP.post("api/packs")
+@doc.summary("API Endpoint to create a new pack")
+@doc.description("API Endpoint to create a new pack.")
+@doc.consumes(
+    doc.JsonBody(
+        {"description": str, "name": str, "owners": [str], "roles": [str]},
+        description="Details for the new pack",
+    ),
+    location="body",
+    required=True,
+    content_type="application/json",
+)
+@doc.produces(
+    {"data": {"name": str, "owners": [str], "pack_id": str, "roles": [str]}},
+    description="Successfully created a new pack",
+    content_type="application/json",
+)
+@doc.response(
+    400, {"message": str, "code": int}, description="Bad Request: Improper JSON format."
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
 @authorized()
 async def create_new_pack(request):
     """Create a new pack"""
@@ -84,6 +145,29 @@ async def create_new_pack(request):
 
 
 @PACKS_BP.get("api/packs/<pack_id>")
+@doc.summary("API Endpoint to get a specific pack")
+@doc.description("API Endpoint to get a specific pack.")
+@doc.consumes({"head": str}, location="query")
+@doc.produces(
+    {
+        "data": {
+            "description": str,
+            "id": str,
+            "name": str,
+            "owners": [str],
+            "roles": [str],
+        },
+        "head": str,
+        "link": str,
+    },
+    description="Successfully gets specific pack",
+    content_type="application/json",
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
 @authorized()
 async def get_pack(request, pack_id):
     """Get a single pack"""
@@ -97,6 +181,19 @@ async def get_pack(request, pack_id):
 
 
 @PACKS_BP.get("api/packs/check")
+@doc.summary("API Endpoint to check if a pack name already exists")
+@doc.description("API Endpoint to check if a pack name already exists.")
+@doc.consumes({"name": str}, location="query")
+@doc.produces(
+    {"exists": bool},
+    description="Response indicating if pack name already exists",
+    content_type="application/json",
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
 @authorized()
 async def check_pack_name(request):
     """Check if a pack exists with provided name"""
@@ -109,6 +206,27 @@ async def check_pack_name(request):
 
 
 @PACKS_BP.post("api/packs/<pack_id>/members")
+@doc.summary("API Endpoint to add a member to specified pack")
+@doc.description("API Endpoint to add a member to specified pack.")
+@doc.consumes(
+    doc.JsonBody({"id": str}),
+    location="body",
+    required=True,
+    content_type="application/json",
+)
+@doc.produces(
+    {"pack_id": str},
+    description="Response with ID of pack",
+    content_type="application/json",
+)
+@doc.response(
+    400, {"message": str, "code": int}, description="Bad Request: Improper JSON format."
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
 @authorized()
 async def add_pack_member(request, pack_id):
     """Add a member to the roles of a pack"""
@@ -127,6 +245,27 @@ async def add_pack_member(request, pack_id):
 
 
 @PACKS_BP.post("api/packs/<pack_id>/roles")
+@doc.summary("API Endpoint to add roles to specified pack")
+@doc.description("API Endpoint to add roles to specified pack.")
+@doc.consumes(
+    doc.JsonBody({"roles": [str]}),
+    location="body",
+    required=True,
+    content_type="application/json",
+)
+@doc.produces(
+    {"roles": [str]},
+    description="Success response contains list of roles added to pack",
+    content_type="application/json",
+)
+@doc.response(
+    400, {"message": str, "code": int}, description="Bad Request: Improper JSON format."
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
 @authorized()
 async def add_pack_role(request, pack_id):
     """Add roles to a pack"""
@@ -140,6 +279,33 @@ async def add_pack_role(request, pack_id):
 
 
 @PACKS_BP.delete("api/packs/<pack_id>")
+@doc.summary("API Endpoint to delete specified pack")
+@doc.description(
+    "API Endpoint to delete specified pack. Only pack owners and Next Admins are allowed to "
+    "delete packs in NEXT."
+)
+@doc.consumes({"pack_id": str}, location="path", required=True)
+@doc.produces(
+    {"message": str, "deleted": int, "id": str},
+    description="Successfully deleted specified pack",
+    content_type="application/json",
+)
+@doc.response(
+    400,
+    {"code": int, "message": str},
+    description="Pack does not exist or has already been deleted.",
+)
+@doc.response(
+    401,
+    {"code": int, "message": str},
+    description="Unauthorized: The request lacks valid authentication credentials.",
+)
+@doc.response(
+    403,
+    {"code": int, "message": str},
+    description="Forbidden: The provided credentials are not authorized to perform "
+    "the requested action.",
+)
 @authorized()
 async def delete_pack(request, pack_id):
     """Delete pack from NEXT
@@ -150,7 +316,7 @@ async def delete_pack(request, pack_id):
             str: ID of pack to delete
     """
     log_request(request)
-    txn_key, txn_user_id = await get_transactor_key(request)
+    _, txn_user_id = await get_transactor_key(request)
 
     conn = await create_connection()
     pack = await packs_query.get_pack_by_pack_id(conn, pack_id)
@@ -160,7 +326,7 @@ async def delete_pack(request, pack_id):
         )
     owners = await packs_query.get_pack_owners_by_id(conn, pack_id)
     if txn_user_id not in owners and not await check_admin_status(txn_user_id):
-        raise ApiBadRequest(
+        raise ApiForbidden(
             "Error: You do not have the authorization to delete this pack."
         )
     await packs_query.delete_pack_by_id(conn, pack_id)
